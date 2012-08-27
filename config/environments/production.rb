54e1c4e262861612d238a9443e0bdf2a35fe1dd3
Peter Kennedy <peterjs.kennedy@mail.utoronto.ca>
2012-08-27 11:26:02 -0400

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,6 @@ Personal::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => 'gmail.com',
     :authentication => :plain,
     :user_name => 'peter.kennedy.inquiries@gmail.com',
     :password => 'Simple12'
