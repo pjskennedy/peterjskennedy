3195c8d64ccc39a0731312befa80d80d36db0c39
pjskennedy <peterjskennedy@gmail.com>
2013-04-07 18:58:28 -0400

    precompiled minus css, javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Personal::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -26,7 +26,7 @@ Personal::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_dispatch.x_sendfile_header = nil
+  # config.action_dispatch.x_sendfile_header = nil
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
