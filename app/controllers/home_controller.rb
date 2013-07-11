36b8fa05d8f24b2efcfad5b07d602e4069eaf5ec
pjskennedy <peterjskennedy@gmail.com>
2013-07-11 01:28:00 -0400

    Added tagline generation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @activity = [
+      "kicking ass",
+      "saving lives",
+      "fighting crime",
+      "Lifting up heavy things and then putting them back down,",
+      "metaphorically",
+      "figuratively",
+      "literally"].sample
+    @color = [
+      "#BA95E8",
+      "#FFFB5D",
+      "#61FFF8",
+      "#95FFA9",
+      "#77C8E8", 
+      "#FF72D2"].sample
   end
 end
