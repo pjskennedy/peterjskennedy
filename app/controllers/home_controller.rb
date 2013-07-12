069ae0b8e0dc91d638c8e842dface7da7407a119
pjskennedy <peterjskennedy@gmail.com>
2013-07-11 22:16:58 -0400

    added icon switching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,27 @@ class HomeController < ApplicationController
       "Lifting up heavy things and then putting them back down,",
       "metaphorically",
       "figuratively",
+      "doing stuff",
+      "engineerorating",
+      "smelling wonderful",
       "literally"].sample
     @color = [
       "#BA95E8",
       "#FFFB5D",
       "#61FFF8",
       "#95FFA9",
-      "#77C8E8", 
       "#FF353F",
       "#FF72D2"].sample
+    @icon = [
+      "coffee",
+      "trophy",
+      "pencil",
+      "anchor",
+      "lightbulb",
+      "glass",
+      "lemon",
+      "folder-open-alt",
+      "globe",
+      "fire-extinguisher"].sample
   end
 end
